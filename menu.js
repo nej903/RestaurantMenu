2f1f7df6211ff47a950b3eb55df8478cd9c921da
= <=>
2020-09-13 15:41:32 -0500

    added fake food items

--- menu.js
+++ menu.js
@@ -14,3 +14,29 @@ const showMenu = (el) => {
   const menuItem = document.getElementById(menuId);
   menuItem.classList.toggle("active");
 };
+
+fetch("food.json")
+  .then((response) => response.json())
+  .then((data) => displayFood(data))
+  .catch((error) => console.log(error));
+
+const displayFood = (data) => {
+  data.forEach((food) => {
+    const { food: content, items } = food;
+    const meal = menus.find((el) => el.id.includes(content));
+    items.forEach((item) => {
+      const { food, price } = item;
+      const pTag = document.createElement("p");
+      pTag.innerText = `${titleCase(food)} - $${parseFloat(price).toFixed(2)}`;
+      meal.appendChild(pTag);
+    });
+  });
+};
+
+const titleCase = (str) => {
+  var strAr = str.toLowerCase().split(" ");
+  for (var i = 0; i < strAr.length; i++) {
+    strAr[i] = strAr[i].charAt(0).toUpperCase() + strAr[i].slice(1);
+  }
+  return strAr.join(" ");
+};
